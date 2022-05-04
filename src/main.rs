825cf30bea717a0fbaf67d4632a733e478395153
Damian Cyntler <damian@cyntler.com>
2022-05-04 22:22:16 +0200

    Add first simple Yew component

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,44 @@
-use std::io;
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+}
+
+struct App {
+    value: i64,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div id="123123123">
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
 
 fn main() {
-  let mut name = String::new();
-
-  println!("Please enter your name: ");
-
-  match io::stdin().read_line(&mut name) {
-    Ok(..) => 0,
-    Err(..) => 1
-  };
-
-  println!("Hello {}", name);
+    yew::start_app::<App>();
 }
